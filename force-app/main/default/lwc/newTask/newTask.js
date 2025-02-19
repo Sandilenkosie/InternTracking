9f7b9a7eeda9287b56fd53fa3c1b0cde0606c6f9
Sandile <sandilenkosie062gmail.com>
2025-02-19 10:53:43 +0200

    Sprint 4

--- force-app/main/default/lwc/newTask/newTask.js
+++ force-app/main/default/lwc/newTask/newTask.js
@@ -136,7 +136,7 @@ export default class NewTaskCreation extends NavigationMixin(LightningElement) {
         const completion = this.template.querySelector('[data-id="completion"]').value;
         const milestone = this.template.querySelector('[data-id="milestone"]').checked
         const assignedTo = this.selectedInterns.map(intern => intern.User__c);
-        // const phaseId = this.selectedPhase.Id;
+        const phaseId = this.selectedPhase.Id;
 
         if (!taskName || !phaseId || !dueDate || !startDate || !completion || assignedTo.length === 0) {
             this.showToast('Error', 'All fields are required.', 'error');
